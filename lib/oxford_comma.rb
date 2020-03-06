675cbd1b62a4b40080782cb3127a68da1b49da45
Austin Redmond <aredmond85@gmail.com>
2020-03-06 13:21:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 #CONVERT STRINGS SEPARATED BY COMMA TO ARRAY WITH MULTIPLE ELEMENTS
 def oxford_comma(array)
   new_array = []
-  new_array = array
+  new_array = array.join
   return new_array
 end
