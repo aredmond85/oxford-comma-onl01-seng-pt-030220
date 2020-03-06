f9b422f8f2dcac1668ded267ad857b4fceca3b8c
Austin Redmond <aredmond85@gmail.com>
2020-03-06 14:02:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,8 +6,8 @@ def oxford_comma(array)
     return array.join(" and ")
   elsif array.length >= 3
     new_array = []
-    new_array = array.pop()
-    new_array2 = new_array.join
-    return array.join(", ") + ", and " + array.push(new_array2)
+    new_array = "and " + array.pop()
+    array.push(new_array)
+    return array.join(", ") 
   end
 end
