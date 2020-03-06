4a239e696ead30bec8536d3b6bf84b76b98b680d
Austin Redmond <aredmond85@gmail.com>
2020-03-06 13:36:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,8 @@
 #CONVERT STRINGS SEPARATED BY COMMA TO ARRAY WITH MULTIPLE ELEMENTS
 def oxford_comma(array)
-  #new_array = []
   if array.length == 1
-    #new_array = array.join
     return array.join
   elsif array.length == 2
-    #new_array = array.join(" and ")
     return array.join(" and ")
   elsif array.length >= 3
     
